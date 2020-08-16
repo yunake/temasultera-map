92e801bc34898797f8f50dc8e4256a0cc3855fbc
Eugene Yunak <yyunak@softserveinc.com>
2020-08-16 19:38:06 +0300

    Updates

---
+++ precache-manifest.0181b8f851afe2fe5c251c5e75ab61c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88078e256253c633b87eb957365fe3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83ccad154f7d4fc8721",
+    "url": "/static/css/2.62cbf31a.chunk.css"
+  },
+  {
+    "revision": "f55cbc16d9f01004c9df",
+    "url": "/static/css/main.e294fa22.chunk.css"
+  },
+  {
+    "revision": "b83ccad154f7d4fc8721",
+    "url": "/static/js/2.58406299.chunk.js"
+  },
+  {
+    "revision": "e96a6aebb65ba82933c37e7c41bab2d1",
+    "url": "/static/js/2.58406299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55cbc16d9f01004c9df",
+    "url": "/static/js/main.cfa3738c.chunk.js"
+  },
+  {
+    "revision": "d132651615dc7f57397f",
+    "url": "/static/js/runtime-main.d0371723.js"
+  },
+  {
+    "revision": "490c8a8d04c95ecc04a713629553790b",
+    "url": "/static/media/map.490c8a8d.jpg"
+  }
+]);
